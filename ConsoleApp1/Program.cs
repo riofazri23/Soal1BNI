c8e51e38dc87d6dc08c6a4cda108526e9044e0de
DESKTOP-4V6A54V\RIO <rfazri7@gmail.com>
2021-12-24 22:13:28 +0700

    soal1 interview user BNI

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void MyMethod(int bilangan)
+        {
+            int counter1 = 2;
+            int counter2 = 3;
+
+            for (int i = 1; i <= bilangan; i++)
+            {
+                for (int j = 1; j <= bilangan + 3; j++)
+                {
+                    Console.Write(j);
+                    if (j == i)
+                    {
+                        Console.Write("**");
+                        j+=2;
+                    }
+                }
+                Console.WriteLine(" ");
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            MyMethod(6);
+
+        }
+    }
+}
